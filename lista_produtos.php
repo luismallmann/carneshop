6cc8b9dba5d3ddd62d501e97394fe62d81c56cf3
Luis Augusto <luis.mallmann@gmail.com>
2018-05-16 22:12:16 -0300

    Ajuste tela que lista os produtos/adiciona o produto no carrinho.

--- lista_produtos.php
+++ lista_produtos.php
@@ -1,40 +1,165 @@
+<?php
+if (isset($_POST) && isset($_POST["produtoX"])) {
+    $produtoX = $_POST["produtoX"];
+    if ($produtoX == 0)
+        echo '<script>alert("Quantidade deve ser diferente de 0 (zero)!")</script>';
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
 
 <!-- chama o arquivo que contem as informacoes do boot strap -->
-<?php require 'btsinclude.html'; ?>
+<?php
+require 'btsinclude.html';
+?>
+<header>
+<div id="box-topo" class="container-1230">
+<div class="topo">
+<a href="http://localhost/carneshop/lista_produtos.php">
+<img src="img/topo.png" alt="CarneShop" title="CarneShop">
+</a>
+</div>
+</div>
+</header>
+</br>
 
 <!-- constroi a linha de produtos-->
-<div class="row">
-	<!--inicia produto-->
-	<div class="col-sm-2">
-		<!-- 2 define o tamanho -->
-		<div class="card">
-			<div class="card-body">
-				<center>
-					<img class="foto do produto" src="img/icon.png"
-						alt="foto do produto">
-					<p class="card-text">nome do produto.</p>
-					<!-- ajustar método envio -->
-					<div class="form-row align-items-center">
-						<div class="col-auto">
-							<label class="sr-only" for="quantidade"></label>
-							<div class="input-group mb-1">
-								<div class="input-group-prepend">
-									<div class="input-group-text">+/-</div>
-								</div>
-								<input type="number" class="form-control" id="quantidade" value="0" min="0">
+<div class="container">
+	<div class="row">
+		<!--inicia produto-->
+		<div class="col">
+			<!-- 2 define o tamanho -->
+			<div class="card">
+				<div class="card-body">
+					<center>
+						<img class="foto do produto" src="img/icon.png"
+							alt="foto do produto">
+						<p class="card-text">nome do produto.</p>
+						<!-- ajustar método envio -->
+
+						<div class="form-row align-items-center">
+							<div class="col-auto">
+
+								<form action="" method="post" name="frmAdicionar">
+									<div class="input-group mb-1">
+										<div class="input-group-prepend">
+											<div class="input-group-text">+/-</div>
+										</div>
+										<input type="number" name="produtoX" class="form-control"
+											id="quantidade" value="0" min="0">
+									</div>
+							
 							</div>
 						</div>
-					</div>
-					<a href="#" class="btn btn-danger">Adicionar</a>
-				</center>
+
+						<button name="add" class="btn btn-danger" type="submit">Adicionar</button>
+						</form>
+					</center>
+				</div>
 			</div>
 		</div>
+		<!--finaliza produto-->
+		<div class="col">
+			<!-- 2 define o tamanho -->
+			<div class="card">
+				<div class="card-body">
+					<center>
+						<img class="foto do produto" src="img/icon.png"
+							alt="foto do produto">
+						<p class="card-text">nome do produto.</p>
+						<!-- ajustar método envio -->
+
+						<div class="form-row align-items-center">
+							<div class="col-auto">
+
+								<form action="" method="post" name="frmAdicionar">
+									<div class="input-group mb-1">
+										<div class="input-group-prepend">
+											<div class="input-group-text">+/-</div>
+										</div>
+										<input type="number" name="produtoX" class="form-control"
+											id="quantidade" value="0" min="0">
+									</div>
+							
+							</div>
+						</div>
+
+						<button name="add" class="btn btn-danger" type="submit">Adicionar</button>
+						</form>
+					</center>
+				</div>
+			</div>
+		</div>
+		<!--finaliza produto-->
+		<div class="col">
+			<!-- 2 define o tamanho -->
+			<div class="card">
+				<div class="card-body">
+					<center>
+						<img class="foto do produto" src="img/icon.png"
+							alt="foto do produto">
+						<p class="card-text">nome do produto.</p>
+						<!-- ajustar método envio -->
+
+						<div class="form-row align-items-center">
+							<div class="col-auto">
+
+								<form action="" method="post" name="frmAdicionar">
+									<div class="input-group mb-1">
+										<div class="input-group-prepend">
+											<div class="input-group-text">+/-</div>
+										</div>
+										<input type="number" name="produtoX" class="form-control"
+											id="quantidade" value="0" min="0">
+									</div>
+							
+							</div>
+						</div>
+
+						<button name="add" class="btn btn-danger" type="submit">Adicionar</button>
+						</form>
+					</center>
+				</div>
+			</div>
+		</div>
+		<!--finaliza produto-->
+		<div class="col">
+			<!-- 2 define o tamanho -->
+			<div class="card">
+				<div class="card-body">
+					<center>
+						<img class="foto do produto" src="img/icon.png"
+							alt="foto do produto">
+						<p class="card-text">nome do produto.</p>
+						<!-- ajustar método envio -->
+
+						<div class="form-row align-items-center">
+							<div class="col-auto">
+
+								<form action="" method="post" name="frmAdicionar">
+									<div class="input-group mb-1">
+										<div class="input-group-prepend">
+											<div class="input-group-text">+/-</div>
+										</div>
+										<input type="number" name="produtoX" class="form-control"
+											id="quantidade" value="0" min="0">
+									</div>
+							
+							</div>
+						</div>
+
+						<button name="add" class="btn btn-danger" type="submit">Adicionar</button>
+						</form>
+					</center>
+				</div>
+			</div>
+		</div>
+		<!--finaliza produto-->
+		
 	</div>
-	<!--finaliza produto-->
 </div>
 <!-- finaliza a linha de produtos-->
 </br>
