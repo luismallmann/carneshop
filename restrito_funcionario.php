6e2fe6faddfb3ca56b4442cff828130d684b40bd
Natan Vidori <natanvidorismo@hotmail.com>
2018-05-27 17:29:27 -0300

    Criação da página onde tera todo o acesso restrito dos funcionários.

---
+++ restrito_funcionario.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+	
+<html>
+<head>
+<meta charset="UTF-8">
+
+<title>Restrito - CarneShop</title>
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-grid.min.css" rel="stylesheet">
+<link href="css/bootstrap-reboot.min.css" rel="stylesheet">
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<?php
+require 'btsinclude.html';
+?>
+	<div id="box-topo" class="container-1230"
+		style="background-color: #8f5227";>
+		<div class="row">
+			<div class="col">
+				<a href="/carneshop/restrito_funcionario.php"> <img
+					src="img/logo.png" alt="CarneShop" title="CarneShop">
+				</a>
+			</div>
+			<div class="col-md-auto">
+			</div>
+			<div class="col">
+				<img src="img/carne.png" alt="CarneShop" title="CarneShop">
+				</a>
+			</div>
+		</div>
+	</div>
+</header>
+<body>
+<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="pills-inicio-tab" data-toggle="pill" href="#pills-inicio" role="tab" aria-controls="pills-inicio" aria-selected="true">Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-estoque-tab" data-toggle="pill" href="#pills-estoque" role="tab" aria-controls="pills-contact" aria-selected="false">estoque produtos</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-clientes-tab" data-toggle="pill" href="#pills-clientes" role="tab" aria-controls="pills-clientes" aria-selected="false">Cadastro clientes</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Cadastro funcionarios</a>
+  </li>
+    <a class="nav-link" href="http://127.0.0.1/carneshop/index.php">Sair</a>
+</ul>
+<div class="tab-content" id="pills-tabContent">
+  <div class="tab-pane fade show active" id="pills-inicio" role="tabpanel" aria-labelledby="pills-inicio-tab">Seja bem vindo ao Sistema Especial de E-commerce que nem funciona do brasil que apoia o apoio apoiado dos camioneiros!<br><br>Suporte com ......</div>
+  <div class="tab-pane fade" id="pills-clientes" role="tabpanel" aria-labelledby="pills-clientes-tab">..2.</div>
+  <div class="tab-pane fade" id="pills-estoque" role="tabpanel" aria-labelledby="pills-estoque-tab">..3.</div>
+  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">..3.</div>
+</div>
+</body>
+</html>
