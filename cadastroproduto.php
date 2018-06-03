b409828bc34f093ce81848177547d96d9f0c5e89
Luis Augusto <luis.mallmann@gmail.com>
2018-06-02 23:07:47 -0300

    - Implementação da conexão com o banco de dados das telas cadastro de produtos e clientes. (Necessários ainda alguns ajustes).

--- cadastroproduto.php
+++ cadastroproduto.php
@@ -1,65 +1,87 @@
+<?php
+require 'dao/usuariodao.php';
+if (isset($_POST) && isset($_POST['nome']) && isset($_POST['estoque']) && isset($_POST['preco'])) {
+    cadastraProduto($_POST);
+} else {
+    echo "erro";
+}
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<!-- configuracao para adiconar o bootstrap e o jquery -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<script src="js/jquery-3.3.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<style>
-		.container {
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-    	}
+<meta charset="UTF-8">
+<!-- chama o topo/cabeçalho e o bootstrap -->
+	<?php
+require 'btsinclude.html';
+?>
+<script
+	src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
+	type="text/javascript"></script>
+<script src="js/jquery.maskMoney.min.js" type="text/javascript"></script>
+<style>
+.container {
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+}
 
-    	.box {
-	        width: 600px;
-    	    height: 600px;
-        	background: #fff;
-    	}
-	</style>
-	<title>Cadastro de produto</title>
+.box {
+	width: 600px;
+	height: 600px;
+	background: #fff;
+}
+</style>
+<script type="text/javascript">
+ $(function() {
+	    $('#moeda').maskMoney({ thousands:'', decimal:'.'});
+	  })
+    </script>
+
+<title>Cadastro de produto</title>
 </head>
 <body>
 	<div class="container">
 		<div class="box">
 			<h3>Cadastro de Produto</h3>
-			<form>
+			<form action="" method="post" name="frmCadastroProduto">
 				<div class="form-row">
 					<div class="col">
-						<label for="inputname">Nome do Produto</label>
-						<input type="text" class="form-control" placeholder="Nome">
+						<label for="inputname">Nome do Produto</label> <input type="text"
+							name="nome" class="form-control" placeholder="Nome"
+							required="required" maxlength="40">
 					</div>
 				</div>
 				<br>
 				<div class="form-row">
 					<div class="col">
-						<label for="inputname">Estoque (Em kg)</label>
-						<input type="numeric" class="form-control" placeholder="Estoque">
+						<label for="inputname">Estoque (Em kg)</label> <input
+							type="number" name="estoque" class="form-control"
+							placeholder="Estoque" required="required" min="0">
 					</div>
 					<div class="col">
-						<label for="inputpreço">Preço por Kg</label>
-						<input type="numeric" class="form-control" placeholder="Preço">
-					</div>
-				</div>
-				<br>
-				<div class="form-row">					
-					<div class="col">
-						<label for="inputimagem">Imagem (500x500 pixels)</label>
-						<input type="file" class="form-control" placeholder="Imagem">
+						<label for="inputpreço">Preço por Kg (R$)</label> <input type="text"
+							name="preco" class="form-control" placeholder="Preço"
+							required="required" min="0" id="moeda">
 					</div>
 				</div>
 				<br>
 				<div class="form-row">
 					<div class="col">
-						<label for="inputdescrição">Descrição</label>
-						<input type="text" class="form-control" placeholder="Descrição">
+						<label for="inputimagem">Imagem (500x500 pixels)</label> <input
+							type="file" name="imagem" class="form-control"
+							placeholder="Imagem" accept="image/*">
+					</div>
+				</div>
+				<br>
+				<div class="form-row">
+					<div class="col">
+						<label for="inputdescrição">Descrição</label> <input type="text"
+							name="descricao" class="form-control" placeholder="Descrição"
+							maxlength="140">
 					</div>
 				</div>
 				<br>
